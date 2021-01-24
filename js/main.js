91f23b43d2693df5eecd8b450772060722d2f2c1
Francisco <cisco.fv.29@gmail.com>
2021-01-24 00:22:35 -0500

    mend

--- js/main.js
+++ js/main.js
@@ -1,23 +1,24 @@
     window.addEventListener("scroll", ()=>{
         const $word = document.querySelector(".habilidades__word"),
                 $excel = document.querySelector(".habilidades__excel"),
-                $ingles = document.querySelector(".habilidades__ingles"),
+                $ingles = document.querySelector(".habilidades__ingles"), 
                 $distanciaIngles = Math.round($ingles.getBoundingClientRect().bottom),
                 $distanciaExcel = Math.round($excel.getBoundingClientRect().bottom),
                 $distanciaWord = Math.round($word.getBoundingClientRect().bottom);
-        if(!$word.classList.contains("habilidades__word__after" ) && $distanciaWord < 710){
+                console.log($distanciaIngles)
+        if(!$word.classList.contains("habilidades__word__after" ) && $distanciaWord < window.outerHeight){
             $word.classList.add("habilidades__word__after");
-        } else if ($word.classList.contains("habilidades__word__after")&& ($distanciaWord <  50 || $distanciaWord > 710)) {
+        } else if ($word.classList.contains("habilidades__word__after")&& ($distanciaWord <  0 || $distanciaWord > window.outerHeight)) {
             $word.classList.remove("habilidades__word__after")
         }         
-        if(!$excel.classList.contains("habilidades__excel__after" ) && $distanciaExcel < 710){
+        if(!$excel.classList.contains("habilidades__excel__after" ) && $distanciaExcel < window.outerHeight){
             $excel.classList.add("habilidades__excel__after");
-        } else if ($excel.classList.contains("habilidades__excel__after")&& ($distanciaExcel <  50 || $distanciaExcel > 720)) {
+        } else if ($excel.classList.contains("habilidades__excel__after")&& ($distanciaExcel <  0 || $distanciaExcel > window.outerHeight)) {
             $excel.classList.remove("habilidades__excel__after")
         }
-        if(!$ingles.classList.contains("habilidades__ingles__after" ) && $distanciaIngles < 710){
+        if(!$ingles.classList.contains("habilidades__ingles__after" ) && $distanciaIngles < window.outerHeight){
             $ingles.classList.add("habilidades__ingles__after");
-        } else if ($ingles.classList.contains("habilidades__ingles__after")&& ($distanciaIngles <  50 || $distanciaIngles > 720)) {
+        } else if ($ingles.classList.contains("habilidades__ingles__after")&& ($distanciaIngles < 0  || $distanciaIngles > window.outerHeight)) {
             $ingles.classList.remove("habilidades__ingles__after")
         }
     })
