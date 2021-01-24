4ca4aa6c6498bcd4de8a4d1deba3c18c2d2a249b
Francisco <cisco.fv.29@gmail.com>
2021-01-23 20:45:52 -0500

    mend

--- js/main.js
+++ js/main.js
@@ -7,17 +7,17 @@
                 $distanciaWord = Math.round($word.getBoundingClientRect().bottom);
         if(!$word.classList.contains("habilidades__word__after" ) && $distanciaWord < 710){
             $word.classList.add("habilidades__word__after");
-        } else if ($word.classList.contains("habilidades__word__after")&& ($distanciaWord <  0 || $distanciaWord > 710)) {
+        } else if ($word.classList.contains("habilidades__word__after")&& ($distanciaWord <  50 || $distanciaWord > 710)) {
             $word.classList.remove("habilidades__word__after")
         }         
         if(!$excel.classList.contains("habilidades__excel__after" ) && $distanciaExcel < 710){
             $excel.classList.add("habilidades__excel__after");
-        } else if ($excel.classList.contains("habilidades__excel__after")&& ($distanciaExcel <  0 || $distanciaExcel > 720)) {
+        } else if ($excel.classList.contains("habilidades__excel__after")&& ($distanciaExcel <  50 || $distanciaExcel > 720)) {
             $excel.classList.remove("habilidades__excel__after")
         }
         if(!$ingles.classList.contains("habilidades__ingles__after" ) && $distanciaIngles < 710){
             $ingles.classList.add("habilidades__ingles__after");
-        } else if ($ingles.classList.contains("habilidades__ingles__after")&& ($distanciaIngles <  0 || $distanciaIngles > 720)) {
+        } else if ($ingles.classList.contains("habilidades__ingles__after")&& ($distanciaIngles <  50 || $distanciaIngles > 720)) {
             $ingles.classList.remove("habilidades__ingles__after")
         }
     })
